f9599d6227e3f47f47e791ab5633de2a3e3fe23d
Primanata Ardhitama <ardhitama@ymail.com>
2011-11-12 15:38:54 +0700

    - Removed unused header - Changed class to typename

--- src/Technique/Singleton.h
+++ src/Technique/Singleton.h
@@ -1,15 +1,13 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
-#include "../Exception/Exception.h"
-
 /*
   How to use:
   Make this as base class of a class that going to be a singleton class
   Note: don't make virtual base class from this class
 */
 
-template<typename T> class Singleton
+template<class T> class Singleton
 {
 public:
     static T& getInstance()

a1dbcee89723ec0f0add71ba3b04c69427a6f35e
Primanata Ardhitama <ardhitama@ymail.com>
2011-11-12 15:46:11 +0700

    - Improved DP analytics - Some fixes

--- src/Algorithm/LineSimplification/DouglasPeucker.h
+++ src/Algorithm/LineSimplification/DouglasPeucker.h
@@ -18,7 +18,7 @@ T must be Point or the derived class of Point
 // analytics
 inline bool isExist(std::vector<int> map, int id)
 {
-    for(int i=0; i<map.size(); ++i)
+    for(unsigned int i=0; i<map.size(); ++i)
     {
         if(map[i] == id)
             return true;
@@ -39,9 +39,9 @@ void dpReduceRecursive(std::vector<T> &vLineIn, std::vector<T> &vLineOut, const 
     {
         for(int k=i+1; k<j; ++k) // find the max point distance from i-j line
         {
-            tmp_dist = Number::pointToLineDistance(vLineIn[i].getX(), vLineIn[i].getY(), vLineIn[i].getZ(),
-                                                   vLineIn[j].getX(), vLineIn[j].getY(), vLineIn[j].getZ(),
-                                                   vLineIn[k].getX(), vLineIn[k].getY(), vLineIn[k].getZ());
+            tmp_dist = hgl::pointToLineDistance(vLineIn[i].getX(), vLineIn[i].getY(), vLineIn[i].getZ(),
+                                                vLineIn[j].getX(), vLineIn[j].getY(), vLineIn[j].getZ(),
+                                                vLineIn[k].getX(), vLineIn[k].getY(), vLineIn[k].getZ());
 
             if(tmp_dist > dist)
             {
@@ -60,25 +60,25 @@ void dpReduceRecursive(std::vector<T> &vLineIn, std::vector<T> &vLineOut, const 
     {
         vLineOut.push_back(vLineIn[i]);
 
-		// analytics
-        for(int l=i+1; l<j; ++l)
-        {
-            //if(l != f)
-            {
-                tmp_dist = Number::pointToLineDistance(vLineIn[i].getX(), vLineIn[i].getY(), vLineIn[i].getZ(),
-                                                       vLineIn[j].getX(), vLineIn[j].getY(), vLineIn[j].getZ(),
-                                                       vLineIn[l].getX(), vLineIn[l].getY(), vLineIn[l].getZ());
-                //int id= vLineIn[l].id;
-                if(idList.insert(vLineIn[l].id).second == true)
-                    std::cout << "id= " << vLineIn[l].id << " " << vLineIn[l].getLatitude() << ", " << vLineIn[l].getLongitude() << " d from line: " << tmp_dist << " m \n";
-                /*if(!isExist(elPair, id))
-                {
-                    elPair.push_back(vLineIn[l].id);
-                    std::cout << "id= " << vLineIn[l].id << " " << vLineIn[l].getLatitude() << ", " << vLineIn[l].getLongitude() << " d from line: " << tmp_dist << " m \n";
-                }*/
-            }
-        }
-		// analytics
+        // analytics
+//        for(int l=i+1; l<j; ++l)
+//        {
+//            //if(l != f)
+//            {
+//                tmp_dist = hgl::pointToLineDistance(vLineIn[i].getX(), vLineIn[i].getY(), vLineIn[i].getZ(),
+//                                                    vLineIn[j].getX(), vLineIn[j].getY(), vLineIn[j].getZ(),
+//                                                    vLineIn[l].getX(), vLineIn[l].getY(), vLineIn[l].getZ());
+//                //int id= vLineIn[l].id;
+//                if(idList.insert(vLineIn[l].id).second == true)
+//                    std::cout << "id= " << vLineIn[l].id << " " << vLineIn[l].getLatitude() << ", " << vLineIn[l].getLongitude() << " d from line: " << tmp_dist << " m \n";
+//                /*if(!isExist(elPair, id))
+//                {
+//                    elPair.push_back(vLineIn[l].id);
+//                    std::cout << "id= " << vLineIn[l].id << " " << vLineIn[l].getLatitude() << ", " << vLineIn[l].getLongitude() << " d from line: " << tmp_dist << " m \n";
+//                }*/
+//            }
+//        }
+        // analytics
     }
 }
 
@@ -86,8 +86,12 @@ void dpReduceRecursive(std::vector<T> &vLineIn, std::vector<T> &vLineOut, const 
 template <typename T>
 std::vector<T> dpSimplify(std::vector<T> &vLineIn, double tolerance)
 {
-	if(tolerance > 5000)
-		DebugInfo("Tolerance >5 km will give lost of accuracy", DebugInfo::Warning);
+    if(vLineIn.size() < 2)
+        return vLineIn;
+
+    if(tolerance > 5000)
+        DebugInfo("Tolerance >5 km will give lost of accuracy", DebugInfo::Warning);
+
     std::vector<T> vLineOut;
 
     dpReduceRecursive<T>(vLineIn, vLineOut, 0, vLineIn.size()-1, tolerance);
